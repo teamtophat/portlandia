5c76674d3758f01edc42d130bda3ad56fd3f68e3
Dyanna Tolman <dyannast@gmail.com>
2018-11-12 15:23:13 -0800

    sign in page working

---
+++ js/html.js
@@ -0,0 +1,15 @@
+
+export default function html(strings, ...values) {
+   
+    let htmlString = String.raw(strings, ...values);
+
+
+    var template = document.createElement('template');
+
+
+    template.innerHTML = htmlString;
+
+    let dom = template.content;
+
+    return dom;
+}
