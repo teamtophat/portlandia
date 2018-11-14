4ffc46a6fb98e1ecb3af5641cca8ca312d84616d
Marcy Silverman <marcysilve225@gmail.com>
2018-11-13 17:49:26 -0800

    more map display

--- js/map-display.js
+++ js/map-display.js
@@ -1,4 +1,4 @@
-var mymap = L.map('mapid').setView([45.5122, -122.6587], 14);
+var mymap = L.map('mapid').setView([45.5122, -122.6587], 12);
 
 L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
@@ -20,4 +20,10 @@ coffeeMarker.bindPopup('<b>Stumptown Coffee!</b><br><a onclick="coffee-link" hre
 
 var bookMarker = L.marker([45.5625569, -122.666]).addTo(mymap);
 bookMarker.bindPopup('<b>Women and Women First!</b><br><a onclick="bookstore-link" href="bookstore-question.html"><img id="bookstore-pic" src="bookstore-pic.jpg"></a>'); 
+
+var recordMarker = L.marker([45.523, -122.684]).addTo(mymap);
+recordMarker.bindPopup('<b> Everyday Music!</b><br><a onclick="record-link" href="records-question.html"><img id="record-pic" src="record-pic.jpg"></a>'); 
+
+var brunchMarker = L.marker([45.522, -122.6416]).addTo(mymap);
+brunchMarker.bindPopup('<b> Screen Door!</b><br><a onclick="brunch-link" href="brunch-question.html"><img id="brunch-pic" src="brunch.jpg"></a>'); 
 
