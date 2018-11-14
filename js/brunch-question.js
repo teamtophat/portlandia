6e2f5e5d272b38c1aca19e58b8c99288bded6902
Abel Quintero <abel.j.quintero@gmail.com>
2018-11-13 16:05:32 -0800

    linked up brunch question with question-apiand cleaned up html

--- js/brunch-question.js
+++ js/brunch-question.js
@@ -1,12 +1,45 @@
-const brunchForm = document.getElementById('brunch-form');
+import html from './html.js';
+import questionApi from './question-api.js';
 
-function brunchSubmit() {
-    brunchForm.addEventListener('submit', event => {
-        
-        event.preventDefault();
-        window.location.href = '/main.html';
-    });
+function makeTemplate() {
+    return html`
+        <form id="brunch-form">
+            <div>
+                <label for="answer1">No! That wait is ridiculous, I’ll just make brunch at home</label>
+                <input type="radio" name="answer" id="answer1" value="1">
+            </div>
+            <div>
+                <label for="answer2">I’m gonna go find another brunch place</label>
+                <input type="radio" name="answer" id="answer2" value="2">
+            </div>
+            <div>
+                <label for="answer3">Of course! Brunch is the highlight of my weekend! How else am I gonna post it on my IG???</label>
+                <input type="radio" name="answer" id="answer3" value="3">
+            </div>
+            <div>
+                <button id="brunch-question">Submit</button>
+            </div>
+        </form>
+    `;
+}
 
-}      
+export default class BrunchForm {
+    constructor(onAdd) {
+        this.onAdd = onAdd;
+    }
+    render() {
+        let dom = makeTemplate();
+        const brunchForm = dom.querySelector('#brunch-form');
+        const elements = brunchForm.elements;
 
-brunchSubmit();
+        brunchForm.addEventListener('submit', event => {
+            event.preventDefault();
+            questionApi.add(elements.answer.value);
+        });
+        return dom;
+    }
+}
+
+const app = new BrunchForm().render();
+const root = document.getElementById('root');
+root.appendChild(app);
