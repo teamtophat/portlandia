035b6c607214cde6c1e19373a7bec778015d890b
Marcy Silverman <marcysilve225@gmail.com>
2018-11-15 16:26:39 -0800

    put music on and added extra icons

---
+++ js/bird-scene.js
@@ -0,0 +1,6 @@
+const form = document.getElementById('bird-scene');
+
+form.addEventListener('click', event => {
+    event.preventDefault();
+    window.location.href = './main.html'; 
+});
---
+++ js/chicken-scene.js
@@ -0,0 +1,6 @@
+const form = document.getElementById('chicken-scene');
+
+form.addEventListener('click', event => {
+    event.preventDefault();
+    window.location.href = './main.html'; 
+});
